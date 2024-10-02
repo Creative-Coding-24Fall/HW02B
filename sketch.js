de6905c78e76fee1ea5a4b863a8932c7fe62c513
Shuangu <wshuang661@gmail.com>
2024-10-02 14:22:52 -0400

    update!

--- sketch.js
+++ sketch.js
@@ -1,6 +1,77 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
 
-function draw() {
+    createCanvas(680, 680);
+  
+    background(234, 237, 232,50);
+    angleMode(DEGREES);
+
+
+    noStroke();
+
+    
+    triangle(100, 145, 130, 30, 156, 145)
+    
+    fill(28,101,66,200);
+    ellipse(405,200,100,100);
+  
+  
+    fill(97,91,126,200);
+    rect(0,70,300,7);
+  
+
+    fill(6,144,109,100);
+    rect(480,250,200,13);
+  
+    
+
+    fill(195,93,124,180);
+    rect(420,30,110,150);
+    
+    fill(213,171,66,180);
+    rect(513,144,57,350);
+  
+    push()
+    translate(450,0);
+    rotate(-30)
+    fill(202,48,52,250);
+    rect(0,0,3,633);
+  pop()
+
+
+    push()
+    translate(120,195)
+    rotate(15);
+    fill(209,102,131);
+    rect(0,0,170,330);
+    pop()
+
+    push()
+    translate(-10,480);
+    rotate(-15)
+    fill(40,77,147,150);
+    rect(0,0,800,30);
+    pop()
+  
+    fill(223,123,66,200);
+    circle(300,480,300);
+  
+    fill(198,55,44);
+    rect(230,0,5,680);
+
+  
+    function draw() {
+      
+  
+
+
+ 
+   
+
+  }
+
+  
+  
+
+
+
 }
